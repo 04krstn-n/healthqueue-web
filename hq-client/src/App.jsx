f2a72da126a95645fcdded8c1102e0567fe3d5c9
04krstn-n <kc7345497@gmail.com>
2026-08-24 13:21:16 +0800

    updated web server and client functionalities

--- hq-client/src/App.jsx
+++ hq-client/src/App.jsx
@@ -3,7 +3,7 @@ import { AuthProvider, useAuth } from './context/AuthContext'
 import AppLayout from './components/layout/AppLayout'
 
 // Public
-import LandingPage from './pages/public/LandingPage'
+import LandingPage from '../public/LandingPage'
 
 // Auth
 import LoginPage from './pages/auth/LoginPage'
