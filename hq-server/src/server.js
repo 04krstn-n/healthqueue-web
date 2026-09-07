7981fda0cfaa086ce6f11e41d8511e48eedf4c9c
04krstn-n <kc7345497@gmail.com>
2026-09-07 11:46:16 +0800

    connect to local

--- hq-server/src/server.js
+++ hq-server/src/server.js
@@ -45,7 +45,6 @@ const io = new Server(server, {
     origin: FRONTEND_ORIGINS,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
-    origin: "http://localhost:3000", // Allow requests from localhost:3000
   },
 });
 
@@ -81,7 +80,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  origin: "http://localhost:3000", // Allow requests from localhost:3000
 }));
 
 app.use(express.json({ limit: '10mb' }));
